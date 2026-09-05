efe113f270624c3bf741ee83d69efecbc6a9a11c
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-09-05 19:20:42 +0545

    v2.2??

--- meow_v2.js
+++ meow_v2.js
@@ -1,4 +1,5 @@
 // Let's first make tit for tat thingy then modify it ig :D
+// I heard another algorithm is little better with smth like tit for tat but if we not winning, we change strat
 
 export default function bot( {history} ) {
     const n = history.length
@@ -6,6 +7,9 @@ export default function bot( {history} ) {
     // yah let's start with cooperate
     if (n === 0) return ["C", null]
 
-    const move = history[n-1].opponent
+    const { you, opponent } = history[n-1]
+
+    const good = (you==="D" && opponent==="C") || (you==="C" && opponent==="C")
+    const move = good ? you: (you==="C" ? "D": "C")
     return [move, null]
 }
