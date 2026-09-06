e72ce30dd56bb46577c09196a63be034b7ddf50e
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-09-06 16:11:12 +0545

    v3.1

---
+++ meow_v3.js
@@ -0,0 +1,41 @@
+// my ideal bottt
+
+// This variable is threshold upto which, the bot plays init move
+const INITIAL = 3;
+
+// init move: (tit for tat)
+function init(prev) {
+    return prev
+}
+
+export default function bot( {history, memory} ) {
+    const n = history.length
+    
+    if (memory==null) {
+        memory = {
+            "type": "n"
+        }
+    }
+
+    // yah let's start with cooperate
+    let move = "C"
+
+    // tit for tat logic
+    if (n<INITIAL && n>0) {
+        move = init(history.at(-1).opponent)
+    }
+
+    // check what it does if we "D"
+    else if (n===INITIAL+1 || n===INITIAL+2) {
+        if (history.every(item => item.opponent === "C")) {
+            move = "D"
+        }
+    }
+
+    // Checks if its always "C"
+    else if (history.every(item => item.opponent === history[0].opponent)) {
+        move = "D"
+    }
+
+    return [move, memory]
+}
--- plan.md
+++ plan.md
@@ -5,3 +5,5 @@ I wana implement memory
 and plan for now:
 
 * for certain move. lets take 3, it will play tit for tat
+
+then at move 4 and 5, it will throw "D" if the opponent keeps on "C".
