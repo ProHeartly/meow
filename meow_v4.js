d943e3ba955a3c16513390f7364d2eff2937e711
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-09-06 17:39:24 +0545

    v4 :D

---
+++ meow_v4.js
@@ -0,0 +1,67 @@
+// my ideal bottt
+// a checker for identifying tit for tat and implementing a problem
+
+// This variable is threshold upto which, the bot plays init move
+const INITIAL = 3;
+
+// init move: (tit for tat)
+function init(prev) {
+    return prev
+}
+
+export default function bot( {history, memory} ) {
+    const n = history.length
+    
+    if (memory==null) {
+        memory = {
+            "type": "n"
+        }
+    }
+
+    // yah let's start with cooperate
+    let move;
+    if (n===0) {
+        return ["C", memory]
+    } else {
+        move = history.at(-1).opponent
+    }
+
+    // tit for tat logic
+    if (n<INITIAL && n>0) {
+        move = init(history.at(-1).opponent)
+    }
+
+    // check what it does if we "D"
+    else if (n===INITIAL+1 || n===INITIAL+2) {
+        if (history.every(item => item.opponent === "C")) {
+            move = "D"
+        }
+    }
+
+    if (n===INITIAL+3) {
+        const isTFT = history.slice(1).every((turn, index) => turn.opponent === history[index].you)
+        if (isTFT) {
+            memory.type = "tft"
+        }
+    }
+
+    // Checks if its always "C"
+    else if (history.every(item => item.opponent === history[0].opponent)) {
+        move = "D"
+    }
+
+    if (memory.type === "tft") {
+        if (history.at(-1).opponent === "D") {
+            memory.type = "n"
+            move = "D"
+        } else {
+            move = "C"
+        }
+
+        if (n>20) {
+            move = "C"
+        }
+    }
+
+    return [move, memory]
+}
--- plan.md
+++ plan.md
@@ -4,6 +4,20 @@ I wana implement memory
 
 and plan for now:
 
-* for certain move. lets take 3, it will play tit for tat
+* for certain move. lets take ITITIAL, it will play tit for tat
 
-then at move 4 and 5, it will throw "D" if the opponent keeps on "C".
+then at move INITIAL + 1 and INITIAL + 2, it will throw "D" if the opponent keeps on "C".
+
+
+
+# thinking:
+when it comes to always deflector, it always loses
+
+lemme think of new plan:
+against tit for tat
+
+wait i think against tit for tat, playing "C" and "D" alternatively is better than "C"
+generally, from what I have known, getting mutual "C" > alternating "D" ig?? but it isn't working... hmmm lets try this concept
+
+nvmmm i did some calculations, and when alternating between "D" and "C", we get 3/2 points and for cooperation we get 2.
+but currently, we are getting 1 due to how the bot works.. lets do something different at INITAL + 3
